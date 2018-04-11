4688651173d9a031b990104b0c904da74546586c
Stefanus Prasetyo Nugroho <stefanusnugroho585@gmail.com>
2018-04-11 12:46:35 +0700

    Create CSRFToken.php

---
+++ classes/CSRFToken.php
@@ -0,0 +1,18 @@
+<?php
+class CSRFToken
+{
+	public function generate_token(){
+		$_SESSION['token']  = md5(date("dmY h:i:s").rand(10000,90000));
+		return $_SESSION['token'];
+	}
+	public function show_tokenHTML(){
+		echo '<input type="hidden" name="token" value="'.$this->generate_token().'"></input>';
+	}
+	public function validateToken($token){
+		if($token != $_SESSION['token']){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
